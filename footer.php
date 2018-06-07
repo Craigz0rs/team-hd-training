bfda4858674a2e952685d5b1a42c78f73946cf46
Craig D'Arcy <cdarcy@live.ca>
2018-06-07 01:51:58 -0700

    Styled footer

--- footer.php
+++ footer.php
@@ -10,17 +10,46 @@
  * @author Craig D'Arcy - Max Rep Media
  */
 
+if (function_exists('get_field')) {
+    if (get_field('instagram', 12)){ $footinstagram = get_field('instagram', 12); }
+    if (get_field('facebook', 12)){ $footfacebook = get_field('facebook', 12); }
+    if (get_field('youtube', 12)){ $footyoutube = get_field('youtube', 12); }  
+    
+}
+
 ?>
 
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Site %1$s by %2$s.', 'team_hd_training' ), '', '<a href="https://maxrepmedia.com">Max Rep Media</a>' );
-				?>
-		</div><!-- .site-info -->
+        <div class="footer_wrapper">
+            <div class="site-info">
+                <p>
+                    <?php
+                    /* translators: 1: Theme name, 2: Theme author. */
+                    printf( esc_html__( 'Site %1$s by %2$s', 'team_hd_training' ), '', '<a href="https://maxrepmedia.com">Max Rep Media</a>' );
+                    ?>
+                </p>
+            </div><!-- .site-info -->
+            <div class="site_copyright">
+                <p>&#169; HD Training 2018<?php if(date('Y') != 2018){ echo ' - ' . date('Y'); } ?></p>
+            </div><!-- .site_copyright -->
+            <?php if($footinstagram || $footyoutube || $footfacebook) { ?>
+            <div class="site_follow">
+                                    <ul>
+                                                <?php if ($footinstagram) { ?>
+                                                <li><a href="<?php echo $footinstagram; ?>"><i class="fab fa-instagram"></i></a></li>
+                                                <?php } 
+                                                if ($footyoutube) { ?>
+                                                <li><a href="<?php echo $footyoutube; ?>"><i class="fab fa-youtube-square"></i></a></li>
+                                                <?php }
+                                                if ($footfacebook) { ?>
+                                                <li><a href="<?php echo $footfacebook; ?>"><i class="fab fa-facebook-square"></i></a></li>
+                                                <?php } ?>
+                                    </ul>        
+            </div><!-- .site_follow -->
+            <?php } ?>
+        </div><!-- .footer_wrapper -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>-->
